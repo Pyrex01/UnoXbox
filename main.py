ff398628824b15afa0f77a4cbdb1cad678f547f0
riyan <khanshafique.ahamed@gmail.com>
2022-02-18 17:26:59 +0530

    created map value function called them in gampad api

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 import vgamepad as vg
 from pyfirmata import Arduino , util
-gamepad = vg.VX360Gamepad()
 import serial.tools.list_ports
+
+
+
+
+def translate(value, leftMin, leftMax, rightMin, rightMax):
+    # Figure out how 'wide' each range is
+    leftSpan = leftMax - leftMin
+    rightSpan = rightMax - rightMin
+
+    # Convert the left range into a 0-1 range (float)
+    valueScaled = float(value - leftMin) / float(leftSpan)
+
+    # Convert the 0-1 range into a value in the right range.
+    return rightMin + (valueScaled * rightSpan)
+
+
+def convertToTrigger(value):
+    return translate(value,0.0,1.0,0,255)
+
+
+def convertToJoystick(value):
+    return translate(value,0.0,1.0,-32768,32767)
 
 ports = list(serial.tools.list_ports.comports())
 
@@ -18,9 +39,21 @@ if len(Arduino_ports) > 1:
 else:
     print("Arduino board detected")
 
+gamepad = vg.VX360Gamepad()
 print(Arduino_ports[0].device)
 
 board = Arduino(str(Arduino_ports[0].device))
 
 it = util.Iterator(board)
 it.start()
+
+analogue1 = board.get_pin("a:0:i")
+analogue2 = board.get_pin("a:1:i")
+analogue3 = board.get_pin("a:2:i")
+
+
+while True:
+    gamepad.left_trigger(convertToTrigger(analogue1.read()))
+    gamepad.right_trigger(convertToTrigger(analogue2.read()))
+    gamepad.left_joystick(convertToJoystick(analogue3.read()))
+    gamepad.update()
